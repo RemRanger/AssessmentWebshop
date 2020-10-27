4795c8b122d79b0005afeea8d914e8be24a6a15f
Rem Blankenburgh <rem.blankenburgh@gmail.com>
2020-10-27 10:47:06 +0100

    Added components in initial state.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BasketComponent } from './basket/basket.component';
 
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule
+  ({
+    declarations:
+      [
+        AppComponent,
+        HomeComponent,
+        NavbarComponent,
+        BasketComponent
+      ],
+    imports:
+      [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule
+      ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
 export class AppModule { }
